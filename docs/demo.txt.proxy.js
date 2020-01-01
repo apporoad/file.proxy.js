41071f6d749b1da4308a0a4712e52d179c6cdc91
apporoad <apporoadina@163.com>
2020-01-01 17:04:03 +0800

    part

---
+++ docs/demo.txt.proxy.js
@@ -0,0 +1,32 @@
+
+exports.operations=[
+    (fileInfo,options) => {
+        
+    }
+]
+exports.appender ={
+    // todo 
+}
+exports.add ={
+    //todo
+}
+exports.replace = [
+    (fileInfo,options)=>{
+        return {
+            from :  /foo/g,
+            to : 'bar'
+        }
+    },
+    (fileInfo,options)=>{
+        return new Promise((r,j)=>{
+            r({
+                from : 'abc',
+                to : "aaa"
+            })
+        })
+    },
+    {
+        from : 'abc',
+        to : 'ccc'
+    }
+]
